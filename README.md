d3f349a0bbb164a4f40217172e869858fa638e3a
rupaku <rupakumari201@yahoo.com>
2021-07-02 23:54:43 +0530

    AWS cdk code and resource creation in python

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # aws-resources-using-cdk
+
+# install aws cli installer
+
+# aws --version
+
+# aws-cli/1.19.104 Python/3.6.0 Windows/10 botocore/1.20.104
+
+# create iam user with programmatic access and existing policy.
+
+# aws configure
+
+<!-- AWS Access Key ID [None]: <type key ID here>
+AWS Secret Access Key [None]: <type access key>
+Default region name [None]: <choose region (e.g. "us-east-1", "eu-west-1")>
+Default output format [None]: <leave blank> -->
+
+# verify node version
+
+# node --version
+
+# npm install -g aws-cdk
+
+# cdk --version
+
+# mkdir cdkworkshop && cd cdkworkshop
+
+# cdk init sample-app --language python
+
+# source .env/bin/activate
+
+# pip install -r requirements.txt
+
+# pip install aws_cdk.aws_dynamodb
+
+# cdk bootstrap # C:\Users\rupak\.aws -- credentials -- correct it
+
+# cdk deploy <stackname> # cdkworkshop
+
+# make new changes and install new package
+
+# cdk diff <stackname>
+
+# cdk deploy
+
+# always install packages
+
+# pip install aws_cdk.aws_apigateway
---
+++ cdkworkshop/cdkworkshop/cdkworkshop_stack.py
@@ -0,0 +1,34 @@
+from aws_cdk import (
+    aws_dynamodb as _dynamodb,
+    aws_kinesis as _kinesis,
+    aws_s3 as _s3,
+    aws_lambda as _lambda,
+    aws_apigateway as _apigateway,
+    core
+)
+
+
+class CdkworkshopStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        my_table = _dynamodb.Table(self, id='dynamoTable',table_name='testcdktable',
+                                    partition_key=_dynamodb.Attribute(name='lastname',type=_dynamodb.AttributeType.STRING))
+
+        my_stream =_kinesis.Stream(self,id='kinesisstream',stream_name='cdkkinesisstream')
+
+        my_bucket = _s3.Bucket(self,id='s3bucket',bucket_name='cdks3bucket')
+
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambdacode'), # create this folder outside stack file folder i.e root level
+            handler='hello.handler',
+        )
+
+        my_api = _apigateway.LambdaRestApi(self,id='lambdaapi',rest_api_name='cdkapi',handler=my_lambda)
+
+
+
+
